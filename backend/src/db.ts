666d81f8995b2ebb82ea14ce87a32b57a4e804f4
sangram-jr <sangrambera58@gmail.com>
2026-02-20 12:42:06 +0530

    add db logic and signup endpoint

---
+++ backend/src/db.ts
@@ -0,0 +1,31 @@
+
+import mongoose, {model, Schema} from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL as string);
+
+
+
+const UserSchema = new Schema({
+    username: {type: String, unique: true},
+    password: String
+})
+
+export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    type: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true },
+})
+
+const LinkSchema = new Schema({
+    hash: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true, unique: true },
+})
+
+export const LinkModel = model("Links", LinkSchema);
+export const ContentModel = model("Content", ContentSchema);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,49 @@
 import express from "express";
+import { UserModel } from "./db.js";
 
 const app = express();
 app.use(express.json());
 
+
+
+
+//for cheaking
 app.get("/", (req, res) => {
   res.json({ message: "hello world" });
 });
 
+
+
+//sign up endpoint
+app.post("/api/v1/signup", async (req, res) => {
+    // TODO: zod validation , hash the password
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        }) 
+
+        res.json({
+            message: "User signed up"
+        })
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists"
+        })
+    }
+})
+
+
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+
+//to run ts for nodejs
+//npm install express
+//npm install -D typescript ts-node @types/node @types/express
+//npx ts-node src/index.ts -->if this not works then --> 1. npm i -D tsx 2. npx tsx src/index.ts
