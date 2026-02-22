81142de9ea8b8a1c1477de8d2ed910ff9d8162d9
sangram-jr <sangrambera58@gmail.com>
2026-02-22 23:57:34 +0530

    add dynamin card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./components/Button"
+import { Card } from "./components/Card"
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
 
@@ -7,9 +8,16 @@ import { ShareIcon } from "./icons/ShareIcon"
 function App() {
   
   return (
-    <div>
-      <Button variant="primary" text="Add Content" startIcon={<PlusIcon />}/>
-      <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon />}/>
+    <div className="p-4">
+      <div className="flex justify-end gap-4">
+          <Button variant="primary" text="Add Content" startIcon={<PlusIcon />}/>
+          <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon />}/> 
+      </div>
+      
+      <div className="flex gap-4">
+        <Card title="first youtube video" link="https://www.youtube.com/watch?v=umejNI-fafg" type="youtube"/>
+        <Card title="first twitter post" link="https://x.com/quantapar/status/2025307568617422929" type="twitter"/>
+      </div>
     </div>
       
   )
