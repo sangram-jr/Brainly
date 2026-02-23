98c4d19feb198e63dcf4cc8a0d82579c983e26f8
sangram-jr <sangrambera58@gmail.com>
2026-02-23 19:57:08 +0530

    connect signin api with frontend

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,13 +1,37 @@
+import { useRef } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export function SignIn(){
+    const usernameRef=useRef<HTMLInputElement>(null);
+    const passwordRef=useRef<HTMLInputElement>(null);
+    const navigate=useNavigate();
+    async function fetchData(){
+        const username=usernameRef.current?.value;
+        const password=passwordRef.current?.value;
+        const response=await axios.post(BACKEND_URL + "/api/v1/signin",{
+            username,
+            password
+        });
+        const jwt=response.data.token;
+        localStorage.setItem("token",jwt);
+        navigate("/dashboard");
+    }
     return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
         <div className="bg-white rounded-xl min-w-48 border border-gray-200 p-8">
-            <Input placeholder="Username"/>
-            <Input placeholder="Password"/>
+            <Input reference={usernameRef} placeholder="Username"/>
+            <Input reference={passwordRef} placeholder="Password"/>
             <div className="flex justify-center items-center pt-4">
-                <Button variant="primary" text="SignIn" fullWidth={true} loading={false}/>
+                <Button 
+                    variant="primary" 
+                    text="SignIn" 
+                    fullWidth={true} 
+                    loading={false}
+                    onClick={fetchData}
+                />
             </div>
 
         </div>
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -3,10 +3,12 @@ import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import axios from 'axios'
 import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export function SignUp(){
     const usernameRef=useRef<HTMLInputElement>(null);
     const PasswordRef=useRef<HTMLInputElement>(null);
+    const navigate=useNavigate();
     async function fetchData(){
         const username=usernameRef.current?.value;
         const password=PasswordRef.current?.value;
@@ -15,6 +17,7 @@ export function SignUp(){
             username,
             password
         });
+        navigate("/signin")
         alert("you are signed up")
         
     }
