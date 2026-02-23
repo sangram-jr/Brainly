cce6ee0ce754be03c5b76a9642095a7f65c50e7f
sangram-jr <sangrambera58@gmail.com>
2026-02-23 13:43:08 +0530

    add sidebar style

--- frontend/src/components/AddContentModal.tsx
+++ frontend/src/components/AddContentModal.tsx
@@ -33,6 +33,6 @@ interface InputProps{
 
 function Input({onChange,placeholder}:InputProps){
     return <div>
-        <input placeholder={placeholder} type={'text'} onChange={onChange} className="px-4 py-2 m-2 rounded border"/>
+        <input placeholder={placeholder} type={'text'} onChange={onChange} className="px-4 py-2 m-2 rounded border border-gray-200"/>
     </div>
 }
---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,5 @@
+export function Sidebar(){
+    return <div className="h-screen w-72 border-r border-gray-200  bg-white fixed left-0 top-0">
+        hi there
+    </div>
+}
