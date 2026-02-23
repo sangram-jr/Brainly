133e1e07cad4b2b2124c2f3b1531030166d6371f
sangram-jr <sangrambera58@gmail.com>
2026-02-23 16:46:51 +0530

    add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 //import Dashboard from "./pages/Dashboard"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { SignIn } from "./pages/SignIn"
 import { SignUp } from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
 
 
 
@@ -8,7 +10,13 @@ import { SignUp } from "./pages/SignUp"
 function App() {
   
   return (
-    <SignIn/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
     
   )
 }
